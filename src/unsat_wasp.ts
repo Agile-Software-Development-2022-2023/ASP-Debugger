68e7a7f378e74baebedfef3cc27f120a33484857
bfrancesco <brunofrancescobarbara@gmail.com>
2022-11-17 00:26:05 +0100

    string of muses obtained from wasp call implemented

--- src/unsat_wasp.ts
+++ src/unsat_wasp.ts
@@ -1,26 +1,52 @@
-import { ChildProcess } from "child_process";
+import { ChildProcess, ExecException, spawn, spawnSync, SpawnSyncReturns } from "child_process";
 
 const { exec } = require('child_process');
 
 
-export default class WaspMuses{
+export default class WaspCaller{
 
     
     sysComm :string;
     constructor(){
         //for now it is linux based
-        this.sysComm = "./wasp --mus=\"";
+        this.sysComm = "./resources/wasp";
     }   
 
-    call_wasp(grounded: string, d_predicates:string[]):string{
+    exec_command(command:string , args: string[], input:string, std_out:boolean ): string {
+        let execProcess: SpawnSyncReturns<string>;
+        try{
+            execProcess = spawnSync(command, args,  { input : input , encoding: 'utf-8'});
+        }
+        catch(err){
+            console.log("output",err)
+            console.log("sdterr",err.stderr.toString())
+            return "";
+        }  
+        console.log(execProcess.stdout);
+        console.log(execProcess.stderr);
+        if(std_out== true){
+            return execProcess.stdout.toString();
+        }
+        return  execProcess.stderr.toString();
+    }
+
+    // to implement
+    parse_result(muses:string): Array<string[]>{
+        //PARSE compute muses exec oupu and return an array of atoms for each muses specified
+        return [];
+    }
+
+    compute_muses(grounded: string, d_predicates:string[], number_of: number= 0):string{
         let command:string;
-        command = grounded.concat(" >", this.sysComm, " ", d_predicates.join(";"),"\"");
-        console.log(command);
-        let child_proc:ChildProcess = exec(command, function (err: any, stdout: any, stderr: any) {
-                console.log(stdout);
-                stdout;
-            });
-        return child_proc.stdout.read()
+        command =  this.sysComm;
+        let mus = "--mus=".concat(d_predicates.join(";"));
+        let output = this.exec_command(command, [ mus,  "-n ".concat(number_of.toString()) ], grounded, true);
+        return output;
+    }
+
+    get_muses(grounded: string, d_predicates:string[], number_of: number= 0):Array<string[]>{
+        let musesObtained = this.compute_muses(grounded, d_predicates, number_of);
+        return this.parse_result(musesObtained);
     }
 
 }
--- test/test_unsat_wasp.ts
+++ test/test_unsat_wasp.ts
@@ -1,20 +1,19 @@
-import WaspMuses from "../src/unsat_wasp";
+import WaspCaller from "../src/unsat_wasp";
 
 import assert from 'assert';
 // Create a test suite (group) called Math
 describe('WASP_CALL', function() {
-    // Test One: A string explanation of what we're testing
-    let wasp_M = new WaspMuses();
+    // Test One: Test of compute muses
+    let wasp_M = new WaspCaller();
     it('should test if the given program return the expected muses', function(){
       let program: string = '1 4 0 0\n1 5 0 0\n1 8 2 0 6 4\n1 9 2 0 7 5\n1 11 3 0 10 9 8\n1 1 2 0 12 8\n1 1 2 0 13 9\n1 1 2 0 14 11\n1 15 0 0\n1 16 0 0\n1 17 0 0\n3 6 6 7 10 12 13 14 0 0\n0\n4 arc(1,2)\n5 arc(2,3)\n6 _debug1(1,2)\n7 _debug1(2,3)\n8 reach(1,2)\n9 reach(2,3)\n10 _debug2(1,2,3)\n11 reach(1,3)\n12 _debug3(1,2)\n13 _debug3(2,3)\n14 _debug3(1,3)\n15 node(1)\n16 node(2)\n17 node(3)\n0\nB+\n0\nB-\n1\n0\n1\n10 _debug2 3 X Z Y reach(X,Y):-reach(X,Z),reach(Z,Y), X!=Y.\n10 _debug3 2 X Y :- reach(X,Y).\n10 _debug1 2 X Y reach(X,Y):-arc(X,Y).\n0';
-      let expected: string = 'WASP 2.0\n\n[MUS #1]: _debug1(1,2) _debug3(1,2)\n[MUS #2]: _debug1(2,3) _debug3(2,3)\n[MUS #3]: _debug1(1,2) _debug1(2,3) _debug2(1,2,3) _debug3(1,3)';
-      let predicates: string[]= ["_debug1", "_debug2", "_debug_3"];
-      console.log(wasp_M.call_wasp(program, predicates));
-      //assert.equal(wasp_M.call_wasp(program, predicates), expected);
+      let expected: string = 'WASP 2.0\n\n[MUS #1]: _debug1(1,2) _debug3(1,2)\n[MUS #2]: _debug1(2,3) _debug3(2,3)\n[MUS #3]: _debug1(1,2) _debug1(2,3) _debug2(1,2,3) _debug3(1,3)\n';
+      let predicates: string[]= ["_debug1", "_debug2", "_debug3"];
+      console.log(wasp_M.compute_muses(program, predicates));
+      assert.equal(wasp_M.compute_muses(program, predicates), expected);
     });
-    // Test Two: A string explanation of what we're testing
-    it('should test if (3-4)*8 = -8', function(){
-      // Our actual test: (3-4)*8 SHOULD EQUAL -8
-      assert.equal(-8, (3-4)*8);
-    });
+
+    //Test Two: Test of parse muses
+
+    //Test Three: Test of get_muses
 });
