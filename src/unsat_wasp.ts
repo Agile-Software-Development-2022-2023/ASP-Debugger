4e4d78fb1a8f6133c4ad8fe97aad88470ad8f71d
bfrancesco <brunofrancescobarbara@gmail.com>
2022-11-18 00:57:57 +0100

    muses parse completed

--- src/unsat_wasp.ts
+++ src/unsat_wasp.ts
@@ -1,11 +1,8 @@
-import { ChildProcess, ExecException, spawn, spawnSync, SpawnSyncReturns } from "child_process";
-
-const { exec } = require('child_process');
+import { spawnSync, SpawnSyncReturns } from "child_process";
 
 
 export default class WaspCaller{
 
-    
     sysComm :string;
     constructor(){
         //for now it is linux based
@@ -20,10 +17,8 @@ export default class WaspCaller{
         catch(err){
             console.log("output",err)
             console.log("sdterr",err.stderr.toString())
-            return "";
+            throw err
         }  
-        console.log(execProcess.stdout);
-        console.log(execProcess.stderr);
         if(std_out== true){
             return execProcess.stdout.toString();
         }
@@ -32,20 +27,56 @@ export default class WaspCaller{
 
     // to implement
     parse_result(muses:string): Array<string[]>{
-        //PARSE compute muses exec oupu and return an array of atoms for each muses specified
-        return [];
+        //PARSE compute muses exec output and return an array of atoms for each muses specified
+        let musMatrix = new  Array<Array<string>>;
+        let muses_lines:string[] = muses.split("\n");
+        let re = new RegExp(/\[MUS\s\#\d+\]\:\s+(.+)/);
+        for(let i:number = 0; i< muses_lines.length;++i) {
+            let el:string = muses_lines[i];
+            let arr = new Array<string>;1
+            if(re.test(el)){
+                //adding a space in order to recognize and add the last token 
+                //assuming that the division happen by a space
+                el = el.replace(re, "$1 ");
+                //if i have only the space, then i don't have a muse
+                if(el.length == 1) continue;
+                let inString:boolean=false;
+                for(let x = 0; x<el.length; x++) { 
+                    if(el[x] == '"' && el[x-1] != '\\'){
+                        inString =  !inString;
+                    }
+                    else if(!inString && el[x] == ' '){
+                        arr.push(el.substring(0, x));
+                        if(x != el.length-1){
+                            el = el.substring(x+1, el.length);
+                            x = 0;
+                        }
+                    }
+                }
+                musMatrix.push(arr);
+            }       
+        }
+        return musMatrix;
     }
 
     compute_muses(grounded: string, d_predicates:string[], number_of: number= 0):string{
         let command:string;
         command =  this.sysComm;
         let mus = "--mus=".concat(d_predicates.join(";"));
-        let output = this.exec_command(command, [ mus,  "-n ".concat(number_of.toString()) ], grounded, true);
+        let output ;
+        try {
+            output = this.exec_command(command, [ mus,  "-n ".concat(number_of.toString()) ], grounded, true);    
+        } catch (error) {
+            throw error
+        }
+        
         return output;
     }
 
     get_muses(grounded: string, d_predicates:string[], number_of: number= 0):Array<string[]>{
-        let musesObtained = this.compute_muses(grounded, d_predicates, number_of);
+        let musesObtained;
+        try{musesObtained = this.compute_muses(grounded, d_predicates, number_of);}
+        catch(e){throw e}
         return this.parse_result(musesObtained);
     }
 
--- test/test_unsat_wasp.ts
+++ test/test_unsat_wasp.ts
@@ -9,11 +9,31 @@ describe('WASP_CALL', function() {
       let program: string = '1 4 0 0\n1 5 0 0\n1 8 2 0 6 4\n1 9 2 0 7 5\n1 11 3 0 10 9 8\n1 1 2 0 12 8\n1 1 2 0 13 9\n1 1 2 0 14 11\n1 15 0 0\n1 16 0 0\n1 17 0 0\n3 6 6 7 10 12 13 14 0 0\n0\n4 arc(1,2)\n5 arc(2,3)\n6 _debug1(1,2)\n7 _debug1(2,3)\n8 reach(1,2)\n9 reach(2,3)\n10 _debug2(1,2,3)\n11 reach(1,3)\n12 _debug3(1,2)\n13 _debug3(2,3)\n14 _debug3(1,3)\n15 node(1)\n16 node(2)\n17 node(3)\n0\nB+\n0\nB-\n1\n0\n1\n10 _debug2 3 X Z Y reach(X,Y):-reach(X,Z),reach(Z,Y), X!=Y.\n10 _debug3 2 X Y :- reach(X,Y).\n10 _debug1 2 X Y reach(X,Y):-arc(X,Y).\n0';
       let expected: string = 'WASP 2.0\n\n[MUS #1]: _debug1(1,2) _debug3(1,2)\n[MUS #2]: _debug1(2,3) _debug3(2,3)\n[MUS #3]: _debug1(1,2) _debug1(2,3) _debug2(1,2,3) _debug3(1,3)\n';
       let predicates: string[]= ["_debug1", "_debug2", "_debug3"];
-      console.log(wasp_M.compute_muses(program, predicates));
       assert.equal(wasp_M.compute_muses(program, predicates), expected);
     });
+ /*   [
+      ["_debug1(1,2)","_debug1(1,3)" "_debug3(1,2)"],
+      ["_debug1(2,3)", "_debug3(2,3)"]
 
+    ]
+*/
     //Test Two: Test of parse muses
+    it('should test if the given muses are divided in the correct way in string', function(){
+      let result: string = 'WASP 2.0\n\n[MUS #1]: _debug1("test \'spaces\' in string works",2) _debug3(1,2)\n[MUS #2]: _debug1(2,3) _debug3(2,3)\n[MUS #3]: _debug1(1,2) _debug1(2,3) _debug2(1,2,3) _debug3(1,3)\n';
+      let expected: string[][] = [['_debug1("test \'spaces\' in string works",2)', '_debug3(1,2)'],['_debug1(2,3)', '_debug3(2,3)'],['_debug1(1,2)', '_debug1(2,3)', '_debug2(1,2,3)', '_debug3(1,3)']];
+      assert.deepEqual(wasp_M.parse_result(result),expected);
+      //assert.equal(wasp_M.compute_muses(program, predicates), expected);
+    });
 
     //Test Three: Test of get_muses
+    it('should test if given a program, get_muses is able to obtain an array of expected muses', function(){
+      let program: string = '1 4 0 0\n1 5 0 0\n1 6 0 0\n1 7 0 0\n8 2 10 11 4 0 8 4 5 6\n8 2 12 13 4 0 9 4 5 7\n1 1 3 0 14 11 13\n1 1 3 0 15 13 11\n1 1 3 0 16 11 13\n1 1 3 0 17 13 11\n1 1 3 1 11 18 6\n1 1 3 1 13 19 7\n3 8 8 9 14 15 16 17 18 19 0 0\n0\n4 column(1)\n5 row(1)\n6 queen(1)\n7 queen(2)\n8 _debug1(1,1,1)\n9 _debug1(2,1,1)\n10 notQ(1,1,1)\n11 q(1,1,1)\n12 notQ(2,1,1)\n13 q(2,1,1)\n14 _debug4(2,1,1,1,1)\n15 _debug4(1,1,1,2,1)\n16 _debug3(2,1,1,1,1)\n17 _debug3(1,1,1,2,1)\n18 _debug2(1)\n19 _debug2(2)\n0\nB+\n0\nB-\n1\n0\n1\n10 _debug4 5 X R C X1 C1 :-q(X,R,C), q(X1,R,C1),X!=X1.\n10 _debug2 1 X :-#count{R,C: q(X,R,C)} != 1, queen(X).\n10 _debug3 5 X R C X1 R1 :-q(X,R,C), q(X1,R1,C),X!=X1.\n10 _debug1 3 X R C q(X,R,C)|notQ(X,R,C):-queen(X), row(R),column(C).\n0\n';
+      let expected: string[][] = [['_debug4(2,1,1,1,1)','_debug2(1)','_debug2(2)'],['_debug4(1,1,1,2,1)', '_debug2(1)', '_debug2(2)'],['_debug3(2,1,1,1,1)', '_debug2(1)','_debug2(2)'],['_debug3(1,1,1,2,1)','_debug2(1)','_debug2(2)']];
+      let predicates: string[]= ["_debug1", "_debug2", "_debug3","_debug4"];
+      assert.deepEqual(wasp_M.get_muses(program,predicates),expected);
+      //assert.equal(wasp_M.compute_muses(program, predicates), expected);
+    });
+
+    ""
+    
 });
