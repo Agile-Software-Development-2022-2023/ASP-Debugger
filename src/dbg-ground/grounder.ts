a2b80459255a80e4638e32cd5c58d27dc2a7d399
Agostino Rizzo <rzzgtn96p08d086j@studenti.unical.it>
2022-11-30 12:21:00 +0100

    Add some ASP utils.

--- src/dbg-ground/grounder.ts
+++ src/dbg-ground/grounder.ts
@@ -1,6 +1,7 @@
 import { spawnSync, SpawnSyncReturns } from 'child_process';
 import { readFileSync } from 'fs';
 import path from 'path';
+import { freezeStrings, make_unique, restoreStrings } from './asp_utils';
 
 export class AspGrounderError extends Error
 {
@@ -59,10 +60,11 @@ export class TheoreticalAspGrounder extends AspGrounder
     
     public ground(inputProgram: string): string
     {
-        inputProgram = this.freezeStrings(inputProgram);
+        let stringsMap: Map<string, string> = new Map<string, string>();
+        inputProgram = freezeStrings(inputProgram, stringsMap);
         inputProgram = this.removeComments(inputProgram);
         inputProgram = this.rewriteFacts(inputProgram);
-        inputProgram = this.restoreStrings(inputProgram);
+        inputProgram = restoreStrings(inputProgram, stringsMap);
         return this.nullifyFactRewritings( this.grounder.ground(inputProgram) );
     }
 
@@ -71,36 +73,11 @@ export class TheoreticalAspGrounder extends AspGrounder
     
     protected rewriteFacts(input_program: string): string
     {
+        //const df_predname: string = this.getDisjFactPredName(input_program);
         return input_program.replace(
             /((?<=((?<!\.)\.(?!\.)))|^)(([ a-zA-Z0-9(),_\-#]|(\.\.))*)(?=((?<!\.)\.(?!\.)))/gm,
             "$3 :- _df") + "\n_df | -_df.";
-    }
-
-    protected freezeStrings(input_program: string): string
-    {
-        let match_count: number = 0;
-        this.stringsMap = new Map<string, string>();
-        let __this: TheoreticalAspGrounder = this;
-        return input_program.replace(/\"(.|\n)*?\"/g, function(match: string)
-            {
-                let string_token: string = '#str-' + (match_count++) + '#';
-                __this.stringsMap.set(string_token, match);
-                return string_token;
-            });
-    }
-
-    protected restoreStrings(input_program: string): string
-    {
-        if ( this.stringsMap.size === 0 )
-            return input_program;
-        let __this: TheoreticalAspGrounder = this;
-        return input_program.replace(/#str-\d+#/g, function(match: string)
-        {
-            if ( !__this.stringsMap.has(match) )
-                return '';
-            return __this.stringsMap.get(match);
-        });
-    }
+    }    
 
     protected nullifyFactRewritings(ground_program: string): string
     {
@@ -141,6 +118,9 @@ export class TheoreticalAspGrounder extends AspGrounder
         }
         catch (err) { return ground_program; }
     }
+
+    protected getDisjFactPredName(input_program: string): string
+        { return make_unique('_df', input_program); }
 
 }
 
