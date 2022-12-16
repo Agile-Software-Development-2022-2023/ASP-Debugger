3d139265030d70b0c2d32b2772e4743e1863b849
Continuous Integration <MatteoPuccio@users.noreply.github.com>
2022-12-16 16:42:56 +0000

    Continuous Integration compilation

--- out/linkings.js
+++ out/linkings.js
@@ -10,7 +10,11 @@ class InvalidLinkingsError extends Error {
 }
 exports.InvalidLinkingsError = InvalidLinkingsError;
 class Linker {
-    //Checks if the given object represents a valid linking object
+    /**
+     * Checks if the given object represents a valid linking object
+     * @param linkings the object to check
+     * @returns true if the given object is a valid linkings object, false otherwise
+    */
     static checkLinkingsValidity(linkings) {
         //Check if the object has the poolId and reverseLinkings property
         if (!("poolId" in linkings) || !("reverseLinkings" in linkings))
@@ -64,21 +68,31 @@ class Linker {
         }
         return true;
     }
-    //Creates or updates a given linkings file by linking the given files (at least 2)
-    //Throws an error if there was a problem reading, writing or checking the existance of the given linkings file
-    //Throws an error if the linkings file contains an invalid linkings object
-    static linkFiles(files_paths, path_to_linkings) {
+    /**
+     * Reads and returns the linkings object given a path
+     * @param path_to_linkings the path to the json file containing the linkings object
+     * @throws an error if there was a problem reading, writing or checking the existance of the given linkings file
+     * @throws {InvalidLinkingsError} if the linkings file contains an invalid linkings object
+     * @returns the read linkings object if the given path exists, otherwise a default empty linkings object
+    */
+    static readAndValidateLinkings(path_to_linkings) {
         let linkings = { "poolId": 0, "reverseLinkings": {} };
-        try {
-            if (fs_1.default.existsSync(path_to_linkings)) {
-                linkings = utils_1.Util.readJSON(path_to_linkings);
-            }
-        }
-        catch (error) {
-            throw error;
+        if (fs_1.default.existsSync(path_to_linkings)) {
+            linkings = utils_1.Util.readJSON(path_to_linkings);
         }
         if (!this.checkLinkingsValidity(linkings))
             throw new InvalidLinkingsError();
+        return linkings;
+    }
+    /**
+     * Creates or updates a given linkings file by linking the given files
+     *
+     * If less than 2 files are given no file is linked
+     * @param files_paths the paths of the files that need to be linked
+     * @param path_to_linkings the path to the json file containing the linkings object
+    */
+    static linkFiles(files_paths, path_to_linkings) {
+        const linkings = this.readAndValidateLinkings(path_to_linkings);
         const reverseLinkings = linkings['reverseLinkings'];
         //Scans through the given files and see if they belong to a pool already
         //The one that belongs to the biggest pool determines the base pool all other linked files will go in
@@ -126,21 +140,12 @@ class Linker {
         }
         utils_1.Util.writeJSON(linkings, path_to_linkings);
     }
-    //Unlinks the given file from any file it is linked to
-    //Throws an error if there was a problem reading, writing or checking the existance of the given linkings file
-    //Throws an error if the linkings file contains an invalid linkings object
-    static unlinkFile(file_path, path_to_linkings) {
-        let linkings = { "poolId": 0, "reverseLinkings": {} };
-        try {
-            if (fs_1.default.existsSync(path_to_linkings)) {
-                linkings = utils_1.Util.readJSON(path_to_linkings);
-            }
-        }
-        catch (error) {
-            throw error;
-        }
-        if (!this.checkLinkingsValidity(linkings))
-            throw new InvalidLinkingsError();
+    /**
+     * Removes a file without validating the given linkings object
+     * @param file_path the path to the file to be unlinked
+     * @param linkings the linkings object (won't be validated)
+    */
+    static unlinkFileNoValidation(file_path, linkings) {
         //If the given file is not linked to any file, do nothing
         if (!(file_path in linkings))
             return;
@@ -157,23 +162,24 @@ class Linker {
             linkedFiles.splice(linkedFiles.indexOf(file_path), 1);
             delete linkings[file_path];
         }
+    }
+    /**
+     * Unlinks the given file from any file it is linked to
+     * @param file_path the path to the file to be unlinked
+     * @param path_to_linkings the path to the json file containing the linkings object
+    */
+    static unlinkFile(file_path, path_to_linkings) {
+        const linkings = this.readAndValidateLinkings(path_to_linkings);
+        this.unlinkFileNoValidation(file_path, linkings);
         utils_1.Util.writeJSON(linkings, path_to_linkings);
     }
-    //Unlinks all files inside the pool that contains the given file and removes that pool
-    //Throws an error if there was a problem reading, writing or checking the existance of the given linkings file
-    //Throws an error if the linkings file contains an invalid linkings object
+    /**
+     * Unlinks all files inside the pool that contains the given file and removes that pool
+     * @param file_path a path to a file contained in the pool to be removed
+     * @param path_to_linkings the path to the json file containing the linkings object
+    */
     static disbandFilePool(file_path, path_to_linkings) {
-        let linkings = { "poolId": 0, "reverseLinkings": {} };
-        try {
-            if (fs_1.default.existsSync(path_to_linkings)) {
-                linkings = utils_1.Util.readJSON(path_to_linkings);
-            }
-        }
-        catch (error) {
-            throw error;
-        }
-        if (!this.checkLinkingsValidity(linkings))
-            throw new InvalidLinkingsError();
+        const linkings = this.readAndValidateLinkings(path_to_linkings);
         //If the given file is not linked to any file, do nothing
         if (!(file_path in linkings))
             return;
@@ -187,42 +193,60 @@ class Linker {
         delete reverseLinkings[pool];
         utils_1.Util.writeJSON(linkings, path_to_linkings);
     }
-    //Returns all pools inside the linkings file
-    //Throws an error if there was a problem reading, writing or checking the existance of the given linkings file
-    //Throws an error if the linkings file contains an invalid linkings object
+    /**
+     * @param path_to_linkings the path to the json file containing the linkings object
+     * @returns an object containing all the pools in the given linkings
+    */
     static getAllPools(path_to_linkings) {
-        let linkings = { "poolId": 0, "reverseLinkings": {} };
-        try {
-            if (fs_1.default.existsSync(path_to_linkings)) {
-                linkings = utils_1.Util.readJSON(path_to_linkings);
-            }
-        }
-        catch (error) {
-            throw error;
-        }
-        if (!this.checkLinkingsValidity(linkings))
-            throw new InvalidLinkingsError();
+        const linkings = this.readAndValidateLinkings(path_to_linkings);
         return linkings["reverseLinkings"];
     }
-    //Returns the array of files linked to the given file
-    //Throws an error if there was a problem reading, writing or checking the existance of the given linkings file
-    //Throws an error if the linkings file contains an invalid linkings object
+    /**
+     * @param file_path a path to a file
+     * @returns if the file is in a pool, the list of paths to all the files in the same pool, otherwise a list containing only the given file
+    */
     static getLinkedFiles(file_path, path_to_linkings) {
-        let linkings = { "poolId": 0, "reverseLinkings": {} };
-        try {
-            if (fs_1.default.existsSync(path_to_linkings)) {
-                linkings = utils_1.Util.readJSON(path_to_linkings);
-            }
-        }
-        catch (error) {
-            throw error;
-        }
-        if (!this.checkLinkingsValidity(linkings))
-            throw new InvalidLinkingsError();
+        const linkings = this.readAndValidateLinkings(path_to_linkings);
         //If the given file is not linked to any file, do nothing
         if (!(file_path in linkings))
             return [file_path];
         return linkings["reverseLinkings"][linkings[file_path]];
+    }
+    /**
+     * Removes the linked files that are missing from the filesystem
+     * @param path_to_linkings the path to the json file containing the linkings object
+     * @param file_path an optional path to a file. If given, removes only the missing files linked to it
+     * @returns the list of paths to the missing files that were removed
+    */
+    static purgeAndGetMissingFiles(path_to_linkings, file_path) {
+        const linkings = this.readAndValidateLinkings(path_to_linkings);
+        let reverseLinkings = linkings["reverseLinkings"];
+        /**
+         * Given a pool, removes the missing files it contains from the linkings
+         * @param pool the pool to check for missing files
+         * @returns the list of paths of the missing files that were removed
+        */
+        const purgePool = function (pool) {
+            let files = reverseLinkings[pool];
+            let missingFiles = [];
+            files.forEach(file => {
+                if (!fs_1.default.existsSync(file))
+                    missingFiles.push(file);
+            });
+            missingFiles.forEach(file => Linker.unlinkFileNoValidation(file, linkings));
+            return missingFiles;
+        };
+        let missingFiles = [];
+        if (file_path) {
+            if (file_path in linkings)
+                missingFiles = missingFiles.concat(purgePool(linkings[file_path]));
+        }
+        else {
+            for (const pool of Object.keys(reverseLinkings))
+                missingFiles = missingFiles.concat(purgePool(pool));
+        }
+        utils_1.Util.writeJSON(linkings, path_to_linkings);
+        return missingFiles;
     }
 }
 exports.Linker = Linker;
