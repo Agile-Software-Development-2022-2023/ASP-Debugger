348698933061bc207bef530acee13e39f8567227
Continuous Integration <MatteoPuccio@users.noreply.github.com>
2023-01-05 17:27:09 +0000

    Continuous Integration compilation

---
+++ out/support/supportAdornment.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SupportAdorner = void 0;
+class SupportAdorner {
+}
+exports.SupportAdorner = SupportAdorner;
+//# sourceMappingURL=supportAdornment.js.map
