6d5e1e516ae253e36d75e9e795b048d87278009d
Continuous Integration <MatteoPuccio@users.noreply.github.com>
2023-01-05 19:46:33 +0000

    Continuous Integration compilation

--- out/support/supportAdornment.d.ts
+++ out/support/supportAdornment.d.ts
@@ -1,2 +1,8 @@
 export declare class SupportAdorner {
+    protected support_predicate: string;
+    protected numeric_program: string;
+    protected output_program: string;
+    constructor(numeric_program: string, support_predicate?: string);
+    addSupport(): string;
+    protected retrieveMax(maps: string): number;
 }
--- out/support/supportAdornment.js
+++ out/support/supportAdornment.js
@@ -2,6 +2,47 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SupportAdorner = void 0;
 class SupportAdorner {
+    constructor(numeric_program, support_predicate = "support") {
+        this.output_program = "";
+        this.numeric_program = numeric_program;
+        this.output_program = "";
+        this.support_predicate = support_predicate;
+    }
+    // appende ad elements[0](le regole) una regola che ha a:- not support(a). per ogni atomo a nella mappa elements[1]
+    // aggiunge support(a) alla mappa di atomi elements[1]
+    addSupport() {
+        let elements = this.numeric_program.split(/^0\n/gm);
+        let support_id = this.retrieveMax(elements[1]);
+        let support_maps = "";
+        let support_rules = "";
+        try {
+            elements[1].split("\n").forEach(atom => {
+                let groups = atom.match(new RegExp("([0-9]+)\\s+(.+)", "m"));
+                if (groups) {
+                    support_rules = support_rules.concat("1 " + groups[1] + " 1 1 " + support_id + "\n");
+                    support_maps = support_maps.concat(support_id + " " + this.support_predicate.concat("(" + groups[2] + ")\n"));
+                    support_id += 1;
+                }
+            });
+            elements[0] = elements[0].concat(support_rules);
+            elements[1] = elements[1].concat(support_maps);
+        }
+        catch (e) {
+            throw new Error("Unable to parse numeric format ground problem during support adornment: " + e.toString());
+        }
+        return elements.join("0\n");
+    }
+    retrieveMax(maps) {
+        let max = 1;
+        let val;
+        maps.split("\n").forEach(atom => {
+            atom.split(" ", 1).forEach(id => {
+                val = Number(id);
+                max = val < max ? max : val + 1;
+            });
+        });
+        return max;
+    }
 }
 exports.SupportAdorner = SupportAdorner;
 //# sourceMappingURL=supportAdornment.js.map
